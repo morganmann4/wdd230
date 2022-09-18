383f8ebbdb434b1724f6bb8ed1b3cc548978b89d
Morgan Mann <momomann4@gmail.com>
2022-09-18 15:05:10 -0600

    fixed javascript

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
 
 var date = new Date();
+var get_year = date.getFullYear()
+document.getElementById("currentYear").innerHTML = get_year;
 var current_date = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+ date.getDate();
 var current_time = date.getHours()+":"+date.getMinutes()+":"+ date.getSeconds();
 var date_time = current_date+" "+current_time;	
