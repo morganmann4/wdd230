d8c35b8cf57a3c7c2f01212edaf28a45996126a7
Morgan Mann <momomann4@gmail.com>
2022-09-17 23:46:46 -0600

    first

---
+++ W1/scripts/script.js
@@ -0,0 +1,8 @@
+
+var date = new Date();
+var current_date = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+ date.getDate();
+var current_time = date.getHours()+":"+date.getMinutes()+":"+ date.getSeconds();
+var date_time = current_date+" "+current_time;	
+document.getElementById("currentDate").innerHTML = date_time;
+
+
