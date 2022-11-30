f0be37000954acfdf3931eaf72054b2c700ddcb3
Morgan Mann <momomann4@gmail.com>
2022-11-29 19:41:40 -0700

    site plan complete

---
+++ week12/week12/scripts/script.js
@@ -0,0 +1,10 @@
+var date = new Date();
+var get_year = date.getFullYear()
+document.querySelector("#currentYear").textContent = get_year;
+
+
+var current_date = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+ date.getDate();
+var current_time = date.getHours()+":"+date.getMinutes()+":"+ date.getSeconds();
+var date_time = current_date+" "+current_time;	
+
+document.querySelector("#currentDate").textContent = date_time;
